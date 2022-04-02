b2086a1cff209ffc8ff2c79183b1759dc6f3ba46
Fernando <fernangd9@gmail.com>
2022-04-02 17:11:29 +0200

    Project startup

---
+++ src/main/java/com/fgiron1/chats_backend/ChatsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fgiron1.chats_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fgiron1/chats_backend/ChatsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fgiron1.chats_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
